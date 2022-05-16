9894f637a17266de4eb7671c0d58a36333eb5e91
dhillonjashan07 <102655916+dhillonjashan07@users.noreply.github.com>
2022-05-16 12:50:31 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,7 @@ export default class footer extends Component {
             <footer className="bg-white border-top p-3 text-muted small">
                 <div className="row align-items-center justify-content-between">
                     <div>
+                        {/* eslint-disable-next-line */}
                         <span className="navbar-brand mr-2"><img height="35" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/><strong>InducedOfficial</strong></span> Copyright © {new Date().getFullYear()}
                         . All rights reserved.
                     </div>
@@ -15,6 +16,93 @@ export default class footer extends Component {
                     </div>
                 </div>
             </footer>
+        </div>
+    )
+  }
+}
+export class Footer1 extends Component {
+  render() {
+    return (
+        <div className="mt-5">
+            <footer className="bg-black border-top p-3 text-muted small">
+                <div className="footer-clean">
+                        <div className="container">
+                        
+                            <div className="row justify-content-center">
+                            <img  height="150" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Platform</h3>
+                                    <ul>
+                                        <li><a href="#">why induced</a></li>
+                                        <li><a href="#">pirecing</a></li>
+                                        <li><a href="#">The devops lifecycle</a></li>
+                                        <li><a href="#">features</a></li>
+                                        <li><a href="#">releases</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Solution</h3>
+                                    <ul>
+                                        <li><a href="#">Enterprise</a></li>
+                                        <li><a href="#">small buisness</a></li>
+                                        <li><a href="#">continous integration</a></li>
+                                        <li><a href="#">public sector</a></li>
+                                        <li><a href="#">Education</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Resources</h3>
+                                    <ul>
+                                        <li><a href="#">blogs</a></li>
+                                        <li><a href="#">install</a></li>
+                                        <li><a href="#">docs</a></li>
+                                        <li><a href="#">develpor protol</a></li>
+                                        <li><a href="#">newsletter</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>support</h3>
+                                    <ul>
+                                        <li><a href="#">Get help</a></li>
+                                        <li><a href="#">contact sales</a></li>
+                                        <li><a href="#">support</a></li>
+                                        <li><a href="#">status</a></li>
+                                        <li><a href="#">customers protol</a></li>
+                                        <li><a href="#">community form</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>company</h3>
+                                    <ul>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Leadership</a></li>
+                                        <li><a href="#">Board of directors</a></li>
+                                        <li><a href="#">Team</a></li>
+                                        <li><a href="#">Press</a></li>
+                                        <li><a href="#">investor relation</a></li>
+                                        <li><a href="#">Handbook</a></li>
+                                        <li><a href="#">Terms of use </a></li>
+                                        <li><a href="#">Privacy policy</a></li>
+                                        <li><a href="#">cookie prefrences</a></li>
+                                        <li><a href="#">Shop</a></li>
+                                        
+                                    </ul>
+                                </div>
+                            
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row align-items-center justify-content-between">
+                    <div>
+                        <span className="navbar-brand mr-3"><img height="35" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/><strong>InducedOfficial</strong></span> Copyright © 2022
+                        . All rights reserved.
+                    </div>
+                    <div>
+                        Made with <a className="text-secondary font-weight-bold" href="https://github.com/IshanSingla">❤️</a> by InducedOfficial
+                    </div>
+                </div>
+
+                </footer>
         </div>
     )
   }
