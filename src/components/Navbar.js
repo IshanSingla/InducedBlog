c3b91f6a44ce46288d52cfb5c9eb80f96399bd05
dhillonjashan07 <102655916+dhillonjashan07@users.noreply.github.com>
2022-05-16 12:46:27 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,109 @@
-import React from "react";
-export default function Navbar() {
-  return (
-    <nav className="topnav navbar navbar-expand-lg navbar-light bg-white fixed-top">
-        <a className="navbar-brand" href="/">
-          <img height="50" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png"/>
-          <strong>Induced Official</strong>
-        </a>
-        <button
-          className="navbar-toggler collapsed"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor02"
-          aria-controls="navbarColor02"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse" id="navbarColor02">
-          <ul className="navbar-nav mr-auto d-flex align-items-center">
-            <li className="nav-item">
-              <a className="nav-link" href="/languages">Languages</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/projects">Projects</a>
-            </li>
-            <li className="nav-item">
-              <div className="dropdown">
-                <a className="nav-link dropbtn">Compilers</a>
-                <div className="dropdown-content">
-                  <a className="dropdown-item" href="/compiler/Python">Python</a>
-                  <a className="dropdown-item" href="/compiler/C">C</a>
-                  <a className="dropdown-item" href="/compiler/Cpp">C++</a>
-                  <a className="dropdown-item" href="/compiler/Php">Php</a>
-                  <a className="dropdown-item" href="/compiler/Dart">Dart</a>
-                </div>
-              </div>
-            </li>
-            <li className="nav-item">
-              <div className="dropdown">
-                <a className="nav-link dropbtn">Posts / News</a>
-                <div className="dropdown-content">
-                  <a className="dropdown-item" href="/posts/tech">Tech</a>
-                  <a className="dropdown-item" href="/posts/cars">Cars</a>
-                </div>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/apis"> Apis </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about"> About  </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/docs"><span className="badge badge-secondary">docs</span></a>
-            </li>
-          </ul>
-        </div>
-      
-        <div className="dropdown">
-          <img className="rounded-circle shadow" src="https://avatars.githubusercontent.com/u/79676239?s=400&u=50ed13d45e13500fd65b3b84b4f64276d6313ea8&v=4" width="50"/>
-          <div className="dropdown-content">
-            <a className="dropdown-item" href="/Profile">Profile</a>
-            <a className="dropdown-item" href="/Logout">Logout</a>
-          </div>
-        </div>
-    
-    </nav>
-  );
-};
+import React, { Component } from 'react'
 
+export default class footer extends Component {
+  render() {
+    return (
+        <div className="mt-5">
+            <footer className="bg-white border-top p-3 text-muted small">
+                <div className="row align-items-center justify-content-between">
+                    <div>
+                        {/* eslint-disable-next-line */}
+                        <span className="navbar-brand mr-2"><img height="35" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/><strong>InducedOfficial</strong></span> Copyright © {new Date().getFullYear()}
+                        . All rights reserved.
+                    </div>
+                    <div>
+                        Made with <a className="text-secondary font-weight-bold" href="https://github.com/IshanSingla">❤️</a> by InducedOfficial
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+  }
+}
+export class Footer1 extends Component {
+  render() {
+    return (
+        <div className="mt-5">
+            <footer className="bg-black border-top p-3 text-muted small">
+                <div className="footer-clean">
+                        <div className="container">
+                        
+                            <div className="row justify-content-center">
+                            <img  height="150" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Platform</h3>
+                                    <ul>
+                                        <li><a href="#">why induced</a></li>
+                                        <li><a href="#">pirecing</a></li>
+                                        <li><a href="#">The devops lifecycle</a></li>
+                                        <li><a href="#">features</a></li>
+                                        <li><a href="#">releases</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Solution</h3>
+                                    <ul>
+                                        <li><a href="#">Enterprise</a></li>
+                                        <li><a href="#">small buisness</a></li>
+                                        <li><a href="#">continous integration</a></li>
+                                        <li><a href="#">public sector</a></li>
+                                        <li><a href="#">Education</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>Resources</h3>
+                                    <ul>
+                                        <li><a href="#">blogs</a></li>
+                                        <li><a href="#">install</a></li>
+                                        <li><a href="#">docs</a></li>
+                                        <li><a href="#">develpor protol</a></li>
+                                        <li><a href="#">newsletter</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>support</h3>
+                                    <ul>
+                                        <li><a href="#">Get help</a></li>
+                                        <li><a href="#">contact sales</a></li>
+                                        <li><a href="#">support</a></li>
+                                        <li><a href="#">status</a></li>
+                                        <li><a href="#">customers protol</a></li>
+                                        <li><a href="#">community form</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-sm-3 col-md-2 item">
+                                    <h3>company</h3>
+                                    <ul>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Leadership</a></li>
+                                        <li><a href="#">Board of directors</a></li>
+                                        <li><a href="#">Team</a></li>
+                                        <li><a href="#">Press</a></li>
+                                        <li><a href="#">investor relation</a></li>
+                                        <li><a href="#">Handbook</a></li>
+                                        <li><a href="#">Terms of use </a></li>
+                                        <li><a href="#">Privacy policy</a></li>
+                                        <li><a href="#">cookie prefrences</a></li>
+                                        <li><a href="#">Shop</a></li>
+                                        
+                                    </ul>
+                                </div>
+                            
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row align-items-center justify-content-between">
+                    <div>
+                        <span className="navbar-brand mr-3"><img height="35" src="https://telegra.ph/file/dff924bcc1144977cb0b5.png" alt='logo'/><strong>InducedOfficial</strong></span> Copyright © 2022
+                        . All rights reserved.
+                    </div>
+                    <div>
+                        Made with <a className="text-secondary font-weight-bold" href="https://github.com/IshanSingla">❤️</a> by InducedOfficial
+                    </div>
+                </div>
+
+                </footer>
+        </div>
+    )
+  }
+}
